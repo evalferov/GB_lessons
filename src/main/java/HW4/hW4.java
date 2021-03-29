63967d78bc1bf741e0d9b96707a51409c879845f
Evgeny <alferov-ep@yandex.ru>
2021-03-29 16:58:26 +0300

    homeWork4 - выполнил

--- src/main/java/HW4/hW4.java
+++ src/main/java/HW4/hW4.java
@@ -3,13 +3,15 @@ package HW4;
 
 import java.util.Random;
 import java.util.Scanner;
-    public class hW4 {
+
+
+public class hW4 {
         public static int SIZE = 3;
         public static int DOTS_TO_WIN = 3;
         public static final char DOT_EMPTY = '•';
         public static final char DOT_X = 'X';
         public static final char DOT_O = 'O';
-        public static char[][] map;
+        public static char[][] MAP;
         public static Scanner sc = new Scanner(System.in);
         public static Random rand = new Random();
         public static void main(String[] args) {
@@ -40,6 +42,43 @@ import java.util.Scanner;
             System.out.println("Игра закончена");
         }
         public static boolean checkWin(char symb) {
+            boolean cols, rows, d1, d2;
+            for (int i=0; i<3; i++) {
+                cols = true;
+                rows = true;
+                d1 = true;
+                d2 = true;
+                for (int j=0; j<3; j++) {
+                    cols &= (MAP[i][j] == symb);
+                    rows &= (MAP[j][i] == symb);
+                    d1 &= MAP[i][i] == symb;
+                    d2 &= MAP[i][SIZE - i - 1] == symb;
+                }
+
+                if (cols || rows) return true;
+            }
+
+            return false;
+        }
+
+        /*{
+            for (int i = 0; i < SIZE; i++) {
+                boolean row = true, column = true, d1 = true, d2 = true;
+                for (int j = 0; j < SIZE; j++) {
+                    row &= MAP[i][j] == symb;
+                    column &= MAP[j][i] == symb;
+                    d1 &= MAP[i][i] == symb;
+                    d2 &= MAP[i][SIZE - i - 1] == symb;
+                }
+                if (row || column || d1 || d2) {
+                    return true;
+                }
+            }
+
+        }*/
+
+
+        /* {
             if(map[0][0] == symb && map[0][1] == symb && map[0][2] == symb) return true;
             if(map[1][0] == symb && map[1][1] == symb && map[1][2] == symb) return true;
             if (map[2][0] == symb && map[2][1] == symb && map[2][2] == symb) return true;
@@ -49,11 +88,11 @@ import java.util.Scanner;
             if (map[0][0] == symb && map[1][1] == symb && map[2][2] == symb) return true;
             if (map[2][0] == symb && map[1][1] == symb && map[0][2] == symb) return true;
             return false;
-        }
+        }*/
         public static boolean isMapFull() {
             for (int i = 0; i < SIZE; i++) {
                 for (int j = 0; j < SIZE; j++) {
-                    if (map[i][j] == DOT_EMPTY) return false;
+                    if (MAP[i][j] == DOT_EMPTY) return false;
                 }
             }
             return true;
@@ -65,7 +104,7 @@ import java.util.Scanner;
                 y = rand.nextInt(SIZE);
             } while (!isCellValid(x, y));
             System.out.println("Компьютер походил в точку " + (x + 1) + " " + (y + 1));
-            map[y][x] = DOT_O;
+            MAP[y][x] = DOT_O;
         }
         public static void humanTurn() {
             int x, y;
@@ -74,18 +113,18 @@ import java.util.Scanner;
                 x = sc.nextInt() - 1;
                 y = sc.nextInt() - 1;
             } while (!isCellValid(x, y)); // while(isCellValid(x, y) == false)
-            map[y][x] = DOT_X;
+            MAP[y][x] = DOT_X;
         }
         public static boolean isCellValid(int x, int y) {
             if (x < 0 || x >= SIZE || y < 0 || y >= SIZE) return false;
-            if (map[y][x] == DOT_EMPTY) return true;
+            if (MAP[y][x] == DOT_EMPTY) return true;
             return false;
         }
         public static void initMap() {
-            map = new char[SIZE][SIZE];
+            MAP = new char[SIZE][SIZE];
             for (int i = 0; i < SIZE; i++) {
                 for (int j = 0; j < SIZE; j++) {
-                    map[i][j] = DOT_EMPTY;
+                    MAP[i][j] = DOT_EMPTY;
                 }
             }
         }
@@ -97,7 +136,7 @@ import java.util.Scanner;
             for (int i = 0; i < SIZE; i++) {
                 System.out.print((i + 1) + " ");
                 for (int j = 0; j < SIZE; j++) {
-                    System.out.print(map[i][j] + " ");
+                    System.out.print(MAP[i][j] + " ");
                 }
                 System.out.println();
             }
